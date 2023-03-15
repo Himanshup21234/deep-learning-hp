bef624a6d954d4f23dbb6eed2076fef8e289acc8
h0p0303 <himanshu.pant@walmart.com>
2023-03-16 00:38:52 +0530

    Fixed the loop issue.

--- custom_discord_bot_chat_gpt/custom_discord_bot_chat_gpt_4.py
+++ custom_discord_bot_chat_gpt/custom_discord_bot_chat_gpt_4.py
@@ -26,14 +26,7 @@ class ChatBot(discord.Client):
         if message.author == self.user:
             return
         cmd, msg, assistant_response = None, None, "Start by using any one of the following query: \n/hpAi\n/chatGpt\n/hpBot"
-        #print(message.content)
         msg = [message.content]
-        # For gpt 4
-        # if message.attachments:
-        #     for attachment in message.attachments:
-        #         image_bytes = await attachment.read()
-        #         msg.append({"image": image_bytes})
-
         for text in ['/hpAi', '/chatGpt', '/hpBot']:
             if msg[0].startswith(text):
                 msg[0] = msg[0].replace(text, '')
@@ -44,18 +37,14 @@ class ChatBot(discord.Client):
             response = openai.Completion.create(
                 #                 response = openai.ChatCompletion.create(
                 model="code-cushman-001",
-                # gpt-3.5-turbo,text-curie-001, code-cushman-001, code-davinci-002,text-davinci-003
                 temperature=0.1,
                 max_tokens=100,
                 top_p=0.05,
                 frequency_penalty=0,
                 presence_penalty=0,
                 prompt=msg[0]
-                #                             message=[{"role":"user","content":input_content}]
             )
-            #                 assistant_response = response['choices'][0]['message']['content']
             assistant_response = response['choices'][0]['text']
-        #         print(assistant_response)
         await message.channel.send(assistant_response)
 
 
